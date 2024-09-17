02ec24f47e5d557bc337dff2e59ed3cf303ea296
rostya <rostyamudrik@proton.me>
2024-09-17 20:14:50 +0300

    db deleted

--- app/user.py
+++ app/user.py
@@ -10,6 +10,7 @@ user = Router()
 
 # user.message.middleware(BaseMiddleware())
 
+
 @user.message(CommandStart())
 async def cmd_start(message: Message):
     await set_user(message.from_user.id, message.from_user.username)
